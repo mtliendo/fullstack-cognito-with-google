3d2b9e9361035cc6dfd82f6ab88476b9379b4f5c
focusotter <mtliendo@focusotter.com>
2023-10-16 13:37:00 -0500

    add logout function provided by amplify

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,17 @@ import { withAuthenticator } from '@aws-amplify/ui-react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-function Home() {
+type HomePageProps = {
+	user: {}
+	signOut: () => void
+}
+function Home({ user, signOut }: HomePageProps) {
+	console.log({ user })
 	return (
 		<main
 			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
 		>
+			<button onClick={signOut}>SignOut</button>
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
 					Get started by editing&nbsp;
